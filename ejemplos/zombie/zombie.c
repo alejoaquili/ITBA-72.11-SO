02e0dda07333a993219eec614d52494702e882c3
Ariel Godio <agodio@Ariels-MacBook-Pro.local>
2022-08-08 15:23:17 -0300

    Add folder ejemplos and zombie

---
+++ ejemplos/zombie/zombie.c
@@ -0,0 +1,35 @@
+/*
+Ilustrar la creación de un proceso zombie
+
+Instrucciones:
+make
+./a.out &
+ps aux
+<esperar primer sleep>
+ps aux
+ */
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  pid_t child_pid = fork();
+
+  if (child_pid == -1){
+    perror("fork");
+    return 1;
+  }
+
+  sleep(10);
+  printf("First sleep done\n");
+
+  if (child_pid == 0)
+    return 0;
+  else
+    sleep(10);
+
+  printf("Second sleep done\n");
+
+  return 0; 
+}
