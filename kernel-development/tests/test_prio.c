4c83003fe085302c931ee79f910b944ddf23060f
Ariel Godio <agodio@Ariels-MacBook-Pro.local>
2025-10-20 09:16:16 -0300

    Updated test_prio

--- kernel-development/tests/test_prio.c
+++ kernel-development/tests/test_prio.c
@@ -1,63 +1,68 @@
-//TODO
-//ESTE TEST NO ESTA ACTUALIZADO CON EL ENUNCIADO 2C-2025
-//EN UNOS DIAS LO ACTUALIZO Y AVISO
-
-//DEPRECATED
-//DEPRECATED
-//DEPRECATED
-//DEPRECATED
-//DEPRECATED
-//DEPRECATED
-//DEPRECATED
-
 #include <stdint.h>
 #include <stdio.h>
 #include "syscall.h"
 #include "test_util.h"
 
-#define MINOR_WAIT 1000000 // TODO: Change this value to prevent a process from flooding the screen
-#define WAIT 10000000      // TODO: Change this value to make the wait long enough to see theese processes beeing run at least twice
-
 #define TOTAL_PROCESSES 3
+
+#define MAX_VALUE 100000000 // TODO: Change as required
+
 #define LOWEST 0  // TODO: Change as required
 #define MEDIUM 1  // TODO: Change as required
 #define HIGHEST 2 // TODO: Change as required
 
 int64_t prio[TOTAL_PROCESSES] = {LOWEST, MEDIUM, HIGHEST};
 
+void zero_to_max() {
+  uint64_t value = 0;
+
+  while (value++ != MAX_VALUE);
+
+  printf("PROCESS %d DONE!\n", my_getpid());
+}
+
 void test_prio() {
   int64_t pids[TOTAL_PROCESSES];
   char *argv[] = {0};
   uint64_t i;
 
-  for (i = 0; i < TOTAL_PROCESSES; i++)
-    pids[i] = my_create_process("endless_loop_print", 0, argv);
-
-  bussy_wait(WAIT);
-  printf("\nCHANGING PRIORITIES...\n");
+  printf("SAME PRIORITY...\n");
 
   for (i = 0; i < TOTAL_PROCESSES; i++)
+    pids[i] = my_create_process("zero_to_max", 0, argv);
+
+  // Expect to see them finish at the same time
+
+  for (i = 0; i < TOTAL_PROCESSES; i++)
+    my_wait(pids[i]);
+
+  printf("SAME PRIORITY, THEN CHANGE IT...\n");
+
+  for (i = 0; i < TOTAL_PROCESSES; i++) {
+    pids[i] = my_create_process("zero_to_max", 0, argv);
     my_nice(pids[i], prio[i]);
+    printf("  PROCESS %d NEW PRIORITY: %d\n", pids[i], prio[i]);
+  }
 
-  bussy_wait(WAIT);
-  printf("\nBLOCKING...\n");
+  // Expect the priorities to take effect
 
   for (i = 0; i < TOTAL_PROCESSES; i++)
+    my_wait(pids[i]);
+
+  printf("SAME PRIORITY, THEN CHANGE IT WHILE BLOCKED...\n");
+
+  for (i = 0; i < TOTAL_PROCESSES; i++) {
+    pids[i] = my_create_process("zero_to_max", 0, argv);
     my_block(pids[i]);
-
-  printf("CHANGING PRIORITIES WHILE BLOCKED...\n");
-
-  for (i = 0; i < TOTAL_PROCESSES; i++)
-    my_nice(pids[i], MEDIUM);
-
-  printf("UNBLOCKING...\n");
+    my_nice(pids[i], prio[i]);
+    printf("  PROCESS %d NEW PRIORITY: %d\n", pids[i], prio[i]);
+  }
 
   for (i = 0; i < TOTAL_PROCESSES; i++)
     my_unblock(pids[i]);
 
-  bussy_wait(WAIT);
-  printf("\nKILLING...\n");
+  // Expect the priorities to take effect
 
   for (i = 0; i < TOTAL_PROCESSES; i++)
-    my_kill(pids[i]);
+    my_wait(pids[i]);
 }
