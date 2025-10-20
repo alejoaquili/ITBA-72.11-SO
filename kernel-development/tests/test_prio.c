dd249451d7e7133f4e653f946dbad0077ee713bc
Ariel Godio <agodio@Ariels-MacBook-Pro.local>
2025-10-20 09:24:51 -0300

    Added parameter to test_prio

--- kernel-development/tests/test_prio.c
+++ kernel-development/tests/test_prio.c
@@ -5,31 +5,37 @@
 
 #define TOTAL_PROCESSES 3
 
-#define MAX_VALUE 100000000 // TODO: Change as required
-
 #define LOWEST 0  // TODO: Change as required
 #define MEDIUM 1  // TODO: Change as required
 #define HIGHEST 2 // TODO: Change as required
 
 int64_t prio[TOTAL_PROCESSES] = {LOWEST, MEDIUM, HIGHEST};
 
+uint64_t max_value = 0;
+
 void zero_to_max() {
   uint64_t value = 0;
 
-  while (value++ != MAX_VALUE);
+  while (value++ != max_value);
 
   printf("PROCESS %d DONE!\n", my_getpid());
 }
 
-void test_prio() {
+uint64_t test_prio(uint64_t argc, char *argv[]) {
   int64_t pids[TOTAL_PROCESSES];
-  char *argv[] = {0};
+  char *ztm_argv[] = {0};
   uint64_t i;
+
+  if (argc != 1)
+    return -1;
+
+  if ((max_value = satoi(argv[0])) <= 0)
+    return -1;
 
   printf("SAME PRIORITY...\n");
 
   for (i = 0; i < TOTAL_PROCESSES; i++)
-    pids[i] = my_create_process("zero_to_max", 0, argv);
+    pids[i] = my_create_process("zero_to_max", 0, ztm_argv);
 
   // Expect to see them finish at the same time
 
@@ -39,7 +45,7 @@ void test_prio() {
   printf("SAME PRIORITY, THEN CHANGE IT...\n");
 
   for (i = 0; i < TOTAL_PROCESSES; i++) {
-    pids[i] = my_create_process("zero_to_max", 0, argv);
+    pids[i] = my_create_process("zero_to_max", 0, ztm_argv);
     my_nice(pids[i], prio[i]);
     printf("  PROCESS %d NEW PRIORITY: %d\n", pids[i], prio[i]);
   }
@@ -52,7 +58,7 @@ void test_prio() {
   printf("SAME PRIORITY, THEN CHANGE IT WHILE BLOCKED...\n");
 
   for (i = 0; i < TOTAL_PROCESSES; i++) {
-    pids[i] = my_create_process("zero_to_max", 0, argv);
+    pids[i] = my_create_process("zero_to_max", 0, ztm_argv);
     my_block(pids[i]);
     my_nice(pids[i], prio[i]);
     printf("  PROCESS %d NEW PRIORITY: %d\n", pids[i], prio[i]);
